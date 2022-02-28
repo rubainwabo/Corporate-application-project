17252f437e13fa376ade3d9a2356af0ae3925843
Rubain Kamegneson Wabo <rubain.kamegneson@student.vinci.be>
2022-02-28 14:38:45 +0100

    create and init of database

--- src/main/java/dal/DalServices.java
+++ src/main/java/dal/DalServices.java
@@ -4,18 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import utils.Config;
 
 public class DalServices {
-    private Connection con=null;
-    private String url = ""; // file properties later
-    private String username = "";
-    private String password = "";
 
-    public DalServices() throws SQLException{
-        con = DriverManager.getConnection(url, username, password);
-    }
+  private Connection con = null;
+  private String url = Config.getProperty("URL");
+  private String pseudo = Config.getProperty("Pseudo");
+  private String mdp = Config.getProperty("MotDePasse");
 
-    public PreparedStatement getPreparedStatement(String query) throws SQLException {
-       return con.prepareStatement(query);
-    }
+  public DalServices() throws SQLException {
+    con = DriverManager.getConnection(url, pseudo, mdp);
+  }
+
+  public PreparedStatement getPreparedStatement(String query) throws SQLException {
+    return con.prepareStatement(query);
+  }
 }

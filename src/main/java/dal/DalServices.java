72c9008195cc0e5ea2361cb67d247f75bfc8ff4c
Rubain Kamegneson Wabo <rubain.kamegneson@student.vinci.be>
2022-02-28 13:51:30 +0100

    creation of the connection

--- src/main/java/dal/DalServices.java
+++ src/main/java/dal/DalServices.java
@@ -1,4 +1,21 @@
 package dal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DalServices {
+    private Connection con=null;
+    private String url = ""; // file properties later
+    private String username = "";
+    private String password = "";
+
+    public DalServices() throws SQLException{
+        con = DriverManager.getConnection(url, username, password);
+    }
+
+    public PreparedStatement getPreparedStatement(String query) throws SQLException {
+       return con.prepareStatement(query);
+    }
 }

9a97e709d422caf1f34ce0ff6a58190d503dc603
mehdi gobbe <mehdigobbe@student.vinci.be>
2022-02-28 13:48:45 +0100

    implementation : configure method of applicationBinder

--- src/main/java/utils/ApplicationBinder.java
+++ src/main/java/utils/ApplicationBinder.java
@@ -1,5 +1,12 @@
 package utils;
 
+import buiseness.factory.BizFactory;
+import buiseness.factory.BizFactoryImpl;
+import buiseness.ucc.UserUCC;
+import buiseness.ucc.UserUCCImpl;
+import dal.services.UserDAO;
+import dal.services.UserDAOImpl;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.ext.Provider;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
@@ -7,5 +14,8 @@ import org.glassfish.hk2.utilities.binding.AbstractBinder;
 public class ApplicationBinder extends AbstractBinder {
 
   protected void configure() {
+    bind(BizFactoryImpl.class).to(BizFactory.class).in(Singleton.class);
+    bind(UserDAOImpl.class).to(UserDAO.class).in(Singleton.class);
+    bind(UserUCCImpl.class).to(UserUCC.class).in(Singleton.class);
   }
 }

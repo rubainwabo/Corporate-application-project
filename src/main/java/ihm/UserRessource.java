7e4b633f829c4bb736f1288ca043c772a75f7162
mehdi gobbe <mehdigobbe@student.vinci.be>
2022-03-24 20:14:01 +0100

    fix : userDAO getUsersByState

--- src/main/java/ihm/UserRessource.java
+++ src/main/java/ihm/UserRessource.java
@@ -10,8 +10,8 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -108,9 +108,9 @@ public class UserRessource {
    * @return a list of user by a specific state
    */
   @GET
-  @Path("list/{state}")
+  @Path("list")
   @Produces(MediaType.APPLICATION_JSON)
-  public List<UserDTO> deniedUserList(@PathParam("state") String state) {
+  public List<UserDTO> deniedUserList(@QueryParam("state") String state) {
     if (state.isBlank()) {
       throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
           .entity("a state is required").type("text/plain").build());

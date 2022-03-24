b2bcd69a0b17cbc6fb30fe4c19c25799582c0485
Mihaila Eduard <eduard.mihaila@student.vinci.be>
2022-03-24 10:13:44 +0100

    fixing checkstyles

--- src/main/java/ihm/UserRessource.java
+++ src/main/java/ihm/UserRessource.java
@@ -5,7 +5,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
@@ -23,76 +27,77 @@ import utils.exception.UserOnHoldException;
 @Singleton
 @Path("/auths")
 public class UserRessource {
-    @Inject
-    private UserUCC myUserUCC;
 
-    /**
-     * allows to connect the user.
-     *
-     * @param body the data that the user has entered put in json format
-     * @return the token associated to the user, otherwise an error in case of failure
-     */
-    @POST
-    @Path("admin")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String adminPage(Object body) {
-        return "oui";
+  @Inject
+  private UserUCC myUserUCC;
+
+  /**
+   * allows to connect the user.
+   *
+   * @param body the data that the user has entered put in json format
+   * @return the token associated to the user, otherwise an error in case of failure
+   */
+  @POST
+  @Path("admin")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public String adminPage(Object body) {
+    return "oui";
+  }
+
+  @POST
+  @Path("login")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public ObjectNode login(JsonNode body) {
+    if (!body.hasNonNull("username") || !body.hasNonNull("password") || !body.hasNonNull(
+        "rememberMe")) {
+      throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
+          .entity("username or password required").type("text/plain").build());
     }
+    // escape characters to avoid XSS injections
+    String username = StringEscapeUtils.escapeHtml4(body.get("username").asText());
+    String password = StringEscapeUtils.escapeHtml4(body.get("password").asText());
+    // allows to know if the user wants to be remembered
+    boolean rememberMe = body.get("rememberMe").asBoolean();
 
-    @POST
-    @Path("login")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public ObjectNode login(JsonNode body) {
-        if (!body.hasNonNull("username") || !body.hasNonNull("password") || !body.hasNonNull(
-                "rememberMe")) {
-            throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-                    .entity("username or password required").type("text/plain").build());
-        }
-        // escape characters to avoid XSS injections
-        String username = StringEscapeUtils.escapeHtml4(body.get("username").asText());
-        String password = StringEscapeUtils.escapeHtml4(body.get("password").asText());
-        // allows to know if the user wants to be remembered
-        boolean rememberMe = body.get("rememberMe").asBoolean();
-
-        if (username.isBlank() || password.isBlank()) {
-            throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-                    .entity("username or password required").type("text/plain").build());
-        }
-        try {
-            return myUserUCC.login(username, password, rememberMe);
-        } catch (UserInvalidException e) {
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
-                    .entity(e.getMessage()).type("text/plain").build());
-        } catch (PasswordOrUsernameException | ReasonForConnectionRefusalException | UserOnHoldException e) {
-            throw new WebApplicationException(Response.status(Status.UNAUTHORIZED)
-                    .entity(e.getMessage()).type("text/plain").build());
-        }
+    if (username.isBlank() || password.isBlank()) {
+      throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
+          .entity("username or password required").type("text/plain").build());
     }
-
-    /**
-     * Refresh the user token.
-     *
-     * @param body the user's data retrieved via his local storage in the front-end
-     * @return the created token, otherwise null in case of error
-     */
-    @POST
-    @Path("refreshToken")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public ObjectNode userRefreshToken(JsonNode body) {
-        if (!body.hasNonNull("refreshToken")) {
-            throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-                    .entity("a token or refreshToken is required").type("text/plain").build());
-        }
-        // escape characters to avoid XSS injections and transforms the received token into text
-        String refreshToken = StringEscapeUtils.escapeHtml4(body.get("refreshToken").asText());
-        try {
-            return myUserUCC.refreshToken(refreshToken);
-        } catch (InvalidTokenException e) {
-            throw new WebApplicationException(Response.status(Status.UNAUTHORIZED)
-                    .entity(e.getMessage()).type("text/plain").build());
-        }
+    try {
+      return myUserUCC.login(username, password, rememberMe);
+    } catch (UserInvalidException e) {
+      throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND)
+          .entity(e.getMessage()).type("text/plain").build());
+    } catch (PasswordOrUsernameException | ReasonForConnectionRefusalException | UserOnHoldException e) {
+      throw new WebApplicationException(Response.status(Status.UNAUTHORIZED)
+          .entity(e.getMessage()).type("text/plain").build());
     }
+  }
+
+  /**
+   * Refresh the user token.
+   *
+   * @param body the user's data retrieved via his local storage in the front-end
+   * @return the created token, otherwise null in case of error
+   */
+  @POST
+  @Path("refreshToken")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public ObjectNode userRefreshToken(JsonNode body) {
+    if (!body.hasNonNull("refreshToken")) {
+      throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
+          .entity("a token or refreshToken is required").type("text/plain").build());
+    }
+    // escape characters to avoid XSS injections and transforms the received token into text
+    String refreshToken = StringEscapeUtils.escapeHtml4(body.get("refreshToken").asText());
+    try {
+      return myUserUCC.refreshToken(refreshToken);
+    } catch (InvalidTokenException e) {
+      throw new WebApplicationException(Response.status(Status.UNAUTHORIZED)
+          .entity(e.getMessage()).type("text/plain").build());
+    }
+  }
 }

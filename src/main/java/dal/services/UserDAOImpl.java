1e5af9d562cafe3709f6b90bebc3a59f34a77036
Rubain Kamegneson Wabo <rubain.kamegneson@student.vinci.be>
2022-03-30 16:16:59 +0200

    Problem with connection fixed, exceptions done

--- src/main/java/dal/services/UserDAOImpl.java
+++ src/main/java/dal/services/UserDAOImpl.java
@@ -6,6 +6,7 @@ import dal.DalBackService;
 import jakarta.inject.Inject;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import utils.exception.FatalException;

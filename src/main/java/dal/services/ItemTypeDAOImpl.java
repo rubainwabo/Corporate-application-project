203f6ef25a4d27fe4f8329eca08cde1283ea66b9
Rubain Kamegneson Wabo <rubain.kamegneson@student.vinci.be>
2022-03-30 17:04:41 +0200

    try catch added in the method register UserUCCImpl.java

--- src/main/java/dal/services/ItemTypeDAOImpl.java
+++ src/main/java/dal/services/ItemTypeDAOImpl.java
@@ -6,7 +6,6 @@ import dal.DalBackService;
 import jakarta.inject.Inject;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;

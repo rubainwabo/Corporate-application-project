2d6ef360a7d2ec05ee2011347f9b9a9122dfe97b
mehdi gobbe <mehdigobbe@student.vinci.be>
2022-03-30 16:55:09 +0200

    fix : filters + test all route

--- src/main/java/filters/FiltersDynamicBindingConfig.java
+++ src/main/java/filters/FiltersDynamicBindingConfig.java
@@ -1,6 +1,7 @@
 package filters;
 
 import ihm.ItemRessource;
+import ihm.ItemTypeRessource;
 import ihm.UserRessource;
 import jakarta.ws.rs.container.DynamicFeature;
 import jakarta.ws.rs.container.ResourceInfo;
@@ -14,7 +15,7 @@ public class FiltersDynamicBindingConfig implements DynamicFeature {
     if (UserRessource.class.equals(resourceInfo.getResourceClass())
         ||
         ItemRessource.class.equals(resourceInfo.getResourceClass()) ||
-        ItemRessource.class.equals(resourceInfo.getResourceClass())) {
+        ItemTypeRessource.class.equals(resourceInfo.getResourceClass())) {
       if (resourceInfo.getResourceMethod().getName().contains("user")) {
         context.register(AuthorizationRequestFilter.class);
       }

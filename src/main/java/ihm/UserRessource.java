1b31df83d0080a5275580d3afcd19e8a98d5f396
mehdi gobbe <mehdigobbe@student.vinci.be>
2022-04-11 16:55:15 +0200

    add : memberRessource -> put all members/admin api call into this class

--- src/main/java/ihm/UserRessource.java
+++ src/main/java/ihm/UserRessource.java
@@ -7,15 +7,12 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import java.util.List;
 import org.apache.commons.text.StringEscapeUtils;
 import utils.TokenService;
 
@@ -33,44 +30,6 @@ public class UserRessource {
 
   @Inject
   private TokenService myTokenService;
-
-  /**
-   * Change the state of a certain user.
-   *
-   * @param body the data that the user has entered put in json format
-   * @return true or false if state successfully changed.
-   */
-  @POST
-  @Path("changeState")
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
-  public boolean adminChangeState(JsonNode body) {
-    System.out.println(body.get("admin"));
-    if (!body.hasNonNull("change_id") || !body.hasNonNull("state")) {
-      throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-          .entity("id field is required").type("text/plain").build());
-    }
-
-    if (!body.get("state").asText().equals("denied") && body.hasNonNull("refusalReason")) {
-      throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-          .entity("You cannot put refusal reason on something else than refused state")
-          .type("text/plain").build());
-    }
-
-    if (body.get("state").asText().equals("denied") && (!body.hasNonNull("refusalReason")
-        || body.get("refusalReason").asText().isBlank())) {
-      throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-          .entity("You have to put your denial reason if you want to deny someone")
-          .type("text/plain").build());
-    }
-    if (body.hasNonNull("refusalReason")) {
-      return myUserUCC.changeState(body.get("change_id").asInt(), body.get("state").asText(),
-          body.get("refusalReason").asText(), body.get("admin").asBoolean());
-    } else {
-      return myUserUCC.changeState(body.get("change_id").asInt(), body.get("state").asText(), "",
-          body.get("admin").asBoolean());
-    }
-  }
 
   /**
    * allows to connect the user.
@@ -107,42 +66,6 @@ public class UserRessource {
   }
 
   /**
-   * Refresh the user token.
-   *
-   * @param body the user's data retrieved via his local storage in the front-end
-   * @return the created token, otherwise null in case of error
-   */
-  @POST
-  @Path("refreshToken")
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
-  public ObjectNode userRefreshToken(JsonNode body) {
-    if (!body.hasNonNull("refreshToken")) {
-      throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-          .entity("a token or refreshToken is required").type("text/plain").build());
-    }
-    // escape characters to avoid XSS injections and transforms the received token into text
-    String refreshToken = StringEscapeUtils.escapeHtml4(body.get("refreshToken").asText());
-    return myUserUCC.refreshToken(refreshToken);
-  }
-
-  /**
-   * retrives to get all the user with a specific state.
-   *
-   * @return a list of user by a specific state
-   */
-  @GET
-  @Path("list")
-  @Produces(MediaType.APPLICATION_JSON)
-  public List<UserDTO> adminListByState(@QueryParam("state") String state) {
-    if (state.isBlank()) {
-      throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST)
-          .entity("a state is required").type("text/plain").build());
-    }
-    return myUserUCC.getUsersByState(state);
-  }
-
-  /**
    * register a user in the DB.
    *
    * @param user the user
@@ -162,8 +85,6 @@ public class UserRessource {
           Response.status(Response.Status.BAD_REQUEST).entity("Lacks of mandatory info")
               .type("text/plain").build());
     }
-
     return myUserUCC.register(user);
-
   }
 }

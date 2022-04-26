3c4eb5a2af3504f8a6d1b380bfb186aee53d6d18
mehdi gobbe <mehdigobbe@student.vinci.be>
2022-04-26 17:47:42 +0200

    bla

--- src/main/java/be/vinci/pae/utils/TokenServiceImpl.java
+++ src/main/java/be/vinci/pae/utils/TokenServiceImpl.java
@@ -6,6 +6,9 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.sql.Time;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 
@@ -29,6 +32,7 @@ public class TokenServiceImpl implements TokenService {
       token = JWT.create().withIssuer("auth0")
           .withClaim("user", id)
           .withExpiresAt(new Date(tokenLifeTime)).sign(algo);
+
     } catch (Exception e) {
       e.printStackTrace();
     }

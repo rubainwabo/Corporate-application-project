630b804bfc5f024b5f8bc5026fc73e37aa08a288
mehdi gobbe <mehdigobbe@student.vinci.be>
2022-04-28 11:07:04 +0200

    change getOneByID userDAO

--- src/main/java/be/vinci/pae/main/Main.java
+++ src/main/java/be/vinci/pae/main/Main.java
@@ -16,11 +16,11 @@ import org.glassfish.jersey.server.ResourceConfig;
  */
 public class Main {
 
-  public static String BASE_URI = Config.getProperty("BaseUri");
-
   static {
     Config.load("prod.properties");
   }
+
+  public static String BASE_URI = Config.getProperty("BaseUri");
 
   // Base URI the Grizzly HTTP server will listen on
   // public static final String BASE_URI = "http://localhost:8080/";

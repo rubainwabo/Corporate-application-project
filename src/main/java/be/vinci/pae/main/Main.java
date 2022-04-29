428bd9e731bd26c1d75c5ec9b5f243a218ca56a3
mehdi gobbe <mehdigobbe@student.vinci.be>
2022-04-29 19:24:43 +0200

    refacto main + add img in memberList

--- src/main/java/be/vinci/pae/main/Main.java
+++ src/main/java/be/vinci/pae/main/Main.java
@@ -17,10 +17,11 @@ import org.glassfish.jersey.server.ResourceConfig;
  */
 public class Main {
 
+  public static String BASE_URI;
+
   static {
     Config.load("prod.properties");
   }
-  public static String BASE_URI = Config.getProperty("BaseUri");
 
   // Base URI the Grizzly HTTP server will listen on
   // public static final String BASE_URI = "http://localhost:8080/";
@@ -54,6 +55,7 @@ public class Main {
 
 
   public static void main(String[] args) throws IOException {
+    BASE_URI = Config.getProperty("BaseUri");
     final HttpServer server = startServer();
     System.out.println(String.format("Jersey app started with endpoints available at "
         + "%s%nHit Ctrl-C to stop it...", BASE_URI));
